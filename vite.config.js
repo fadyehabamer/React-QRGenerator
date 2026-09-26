f1780ca52a4fb9d222e6a151b69b59bf3a951840
fadyehabamer <fadyamer45@gmail.com>
2026-09-26 16:15:42 +0300

    chore: configure vitest and eslint flat config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.js'],
+  },
 })
